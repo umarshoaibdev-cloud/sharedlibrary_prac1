9840a74131d3b4f8fee76ff199992572e555f420
umarshoaibdev-cloud <umarshoaibdev@gmail.com>
2026-01-09 15:39:41 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -6,3 +6,7 @@ def buildArtifact()
 {
   sh 'mvn package'
 }
+def deployToTomcat(job_name,ip_address,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${job_name}/webapp/target/webapp.war ubuntu@${ip_address}:/var/lib/tomcat10/webapps/${context}.jar"
+}
